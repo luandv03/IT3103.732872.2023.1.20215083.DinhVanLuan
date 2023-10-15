c48dbcc327bc2ea6f7096e87162a64d821119df5
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-10-15 18:44:32 +0700

    complete excercise 6.3

---
+++ lab_1/excerises/bai_6_3/TriangleStar.java
@@ -0,0 +1,31 @@
+package lab_1.excerises.bai_6_3;
+
+import java.util.Scanner;
+
+public class TriangleStar {
+    public static void main(String[] args) {
+        try(Scanner in = new Scanner(System.in)) {
+            System.out.println("Enter height of triangle:");
+            int nHeight = in.nextInt();
+            int nStarMax = 2*nHeight - 1;
+            int nStar;
+            for (int i = 0; i < nHeight; i++) {
+                nStar = 2*i + 1;
+                for (int j = 0; j < nStarMax; j++) {
+                    System.out.print(" ");// print space .
+                    // start position to print '*'.
+                    if (j == (nStarMax - nStar)/2) {
+                        for (int k = 0; k < nStar; k++) {
+                            System.out.print("*");
+                        }
+                        break; // after print number of stars every time then break;
+                    }
+                }
+                System.out.print("\n");
+            }
+            
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
