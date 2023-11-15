4165fc7a1c06edc3ba010b5e10f7edbb59cacd61
Đinh Văn Luận <dinhvanluan2k3@gmail.com>
2023-11-15 14:49:00 +0700

    method print, searchDVDsById, searchDVDsByTitle method in Cart Class

--- lab_2/Cart.java
+++ lab_2/Cart.java
@@ -65,4 +65,51 @@ public class Cart {
         return sum;
     }
 
+    // Đinh Văn Luận - 20215083
+    public void print() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+
+        double totalCost = 0; 
+        for (int i = 0; i < qtyOrdered; i++) {
+            totalCost += itemsOrdered[i].getCost();
+            System.out.println(itemsOrdered[i].toString());
+        }
+
+        System.out.println("Total cost: " + totalCost);
+    }
+
+    public void searchDVDsById(int id) {
+        boolean check = false; // biến check này để kiểm tra sau khi duyệt hết danh sách thì có tìm được DVD cần tìm không
+
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                check = true;
+
+                System.out.println(itemsOrdered[i].toString());
+                break;
+            }
+        }
+
+        if (!check) {
+            System.out.println("Not DVDs match for id = " + id);
+        }
+    }
+
+    public void searchDVDsByTitle(String title) {
+        boolean check = false; // biến check này để kiểm tra sau khi duyệt hết danh sách thì có tìm được DVD cần tìm không
+
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].isMatch(title)) {
+                check = true;
+
+                System.out.println(itemsOrdered[i].toString());
+                break;
+            }
+        }
+
+        if (!check) {
+            System.out.println("Not DVDs match! for title = " + title);
+        }
+    }
 }
